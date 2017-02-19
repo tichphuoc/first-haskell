59d6185b9e914d0e26251003f923018e40830e40
tichphuoc <trantichphuoc@yahoo.com>
2017-02-19 14:59:45 +1100

    Make output a tuple (Float,Float)

--- _solve_quadratic.hs
+++ _solve_quadratic.hs
@@ -1,5 +1,5 @@
 module Solve_Quadratic
     where
 
-solve_quadratic :: Float -> Float -> Float -> Float
-solve_quadratic a b c = 0.0
+solve_quadratic :: Float -> Float -> Float -> (Float Float)
+solve_quadratic a b c = (0.0,0.0)
